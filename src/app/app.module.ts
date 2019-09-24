385f917ebf935e77008dac426d9b5b7582b471ac
dustyjay <janetarisah01@gmail.com>
2019-09-24 11:22:46 +0100

    added form actions to all forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { FooterComponent } from './footer/footer.component';
         HeaderComponent,
         AboutComponent,
         ContactComponent,
-        FooterComponent
+        FooterComponent,
+        SubscriptionComponent
     ],
     imports: [
         BrowserModule,
