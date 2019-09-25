e7c1906121c4f260a9cccb70366e80dcb50a34d8
dustyjay <janetarisah01@gmail.com>
2019-09-25 21:29:00 +0100

    removed all site forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'contact-us', component: ContactComponent }
+    { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppComponent } from './base/app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { SubscriptionComponent } from './subscription/subscription.component';
 
 @NgModule({
     declarations: [
@@ -16,9 +14,7 @@ import { SubscriptionComponent } from './subscription/subscription.component';
         HomeComponent,
         HeaderComponent,
         AboutComponent,
-        ContactComponent,
-        FooterComponent,
-        SubscriptionComponent
+        FooterComponent
     ],
     imports: [
         BrowserModule,
